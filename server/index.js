61bf1bd9b722bbb12c7ec7edd8f0863fdf37fa26
ThienPPham <thienversiopp129.it.vn@gmail.com>
2024-05-25 20:49:52 +0700

    User Routes Setup

--- server/index.js
+++ server/index.js
@@ -8,9 +8,11 @@ import helmet from "helmet";
 import morgan from "morgan";
 import path from "path";
 import { fileURLToPath } from "url";
-import authRoutes from "./routes/auth.js"
+import authRoutes from "./routes/auth.js";
+import userRoutes from "./routes/users.js";
+import postRostes from "./routes/posts.js"
 import { register } from "./controllers/auth.js";
-import { verifyToken } from "./middleware/auth.js";
+
 
 // CONFIGURATIONS
 const __filename = fileURLToPath(import.meta.url);
@@ -44,6 +46,9 @@ app.post("/auth/register", upload.single("picture"), register);
 
 // ROUTES
 app.use("/auth", authRoutes);
+app.use("/users", userRoutes);
+app.use("/posts", postRoutes);
+
 
 // MONGOOSE SETUP
 const PORT = process.env.PORT || 6001;
