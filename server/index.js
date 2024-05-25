a5775ae36c8b4ea3aa60fe65c2eea94be334f1b2
ThienPPham <thienversiopp129.it.vn@gmail.com>
2024-05-25 19:04:28 +0700

    Authentication and Authorization in Node

--- server/index.js
+++ server/index.js
@@ -8,7 +8,9 @@ import helmet from "helmet";
 import morgan from "morgan";
 import path from "path";
 import { fileURLToPath } from "url";
-import { error } from "console";
+import authRoutes from "./routes/auth.js"
+import { register } from "./controllers/auth.js";
+import { verifyToken } from "./middleware/auth.js";
 
 // CONFIGURATIONS
 const __filename = fileURLToPath(import.meta.url);
@@ -35,6 +37,13 @@ const storage = multer.diskStorage({
 });
 
 const upload = multer({ storage });
+
+
+// ROUTES WITH FILES
+app.post("/auth/register", upload.single("picture"), register);
+
+// ROUTES
+app.use("/auth", authRoutes);
 
 // MONGOOSE SETUP
 const PORT = process.env.PORT || 6001;
