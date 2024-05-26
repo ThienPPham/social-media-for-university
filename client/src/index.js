efb3b3a2a1753b73dd1a441bc386bfce2217f51a
ThienPPham <thienversiopp129.it.vn@gmail.com>
2024-05-26 09:19:54 +0700

    Color, Theme, Dark Mode, and Styling Setup

--- client/src/index.js
+++ client/src/index.js
@@ -1,8 +1,8 @@
-import React, { version } from 'react';
+import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import authReducer from "./state"
+import authReducer from "./state";
 import { configureStore } from '@reduxjs/toolkit';
 import { Provider } from 'react-redux';
 import { FLUSH, PAUSE, PERSIST, PURGE, REGISTER, REHYDRATE, persistReducer, persistStore } from 'redux-persist';
@@ -11,6 +11,7 @@ import { PersistGate } from 'redux-persist/integration/react';
 
 const persistConfig = { key: "root", storage, version: 1 };
 const persistedReducer = persistReducer(persistConfig, authReducer);
+
 const store = configureStore({
   reducer: persistedReducer,
   middleware: (getDefaultMiddleware) =>
@@ -21,11 +22,13 @@ const store = configureStore({
     }),
 });
 
+const persistor = persistStore(store);
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <PersistGate loading={null} persistor={persistStore}>
+      <PersistGate loading={null} persistor={persistor}>
         <App />
       </PersistGate>
     </Provider>
--- client/src/scenes/navbar/index.jsx
+++ client/src/scenes/navbar/index.jsx
@@ -1,4 +1,21 @@
+import { useTheme } from "@emotion/react";
+import { Close, DarkMode, Help, LightMode, Menu, Message, Notifications, Search } from "@mui/icons-material";
+import { Box, FormControl, IconButton, InputBase, MenuItem, Select, Typography, useMediaQuery } from "@mui/material";
+import FlexBetween from "components/FlexBetween";
+import { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
+import { setLogout, setMode } from "state";
+
 const Navbar = () => {
+    const [isMobileMenuToggled, setIsMobileMenuToggled] = useState(false);
+    const dispatch = useDispatch();
+    const navigate = useNavigate();
+    const user = useSelector((state) => state.user);
+    const isNonMobileScreens = useMediaQuery("(min-width: 1000px)");
+
+    const theme = useTheme();
+
     return <div>Navbar</div>;
 };
 
